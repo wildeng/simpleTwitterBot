f8b4eb56e128703bfc60c6b35e9c4fa1e4c3b238
wildeng <wildeng@hotmail.com>
2014-09-07 13:57:06 +0000

    constants template to use with the simple bot

---
+++ constants_template.rb
@@ -0,0 +1,10 @@
+class Constants
+        
+        CONSUMER_KEY = 'your key'
+        CONSUMER_SECRET = 'your secret'
+        ACCESS_TOKEN = 'your token'
+        ACCESS_TOKEN_SECRET = 'your token secret'
+
+        USERNAME = 'username'
+        TWITPASS = 'password'
+end
